33fbfae3b4c0a14d8c6d715d764a0c4009f318a4
Vasyl L Polyak <frvpolyak@gmail.com>
2025-05-01 00:10:00 -0400

    Final fix: all tests passing, ready for submission

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -2,6 +2,8 @@ package hexlet.code;
 
 import hexlet.code.schemas.BaseSchema;
 import hexlet.code.schemas.MapSchema;
+import hexlet.code.schemas.StringSchema;
+import hexlet.code.schemas.NumberSchema;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;

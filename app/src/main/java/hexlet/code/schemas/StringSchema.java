85fb3c55bb9ea7e2af484379b2445acd45f56eb1
Vasyl L Polyak <frvpolyak@gmail.com>
2025-08-09 00:46:37 -0400

    fix(StringSchema): empty is valid when not required

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -11,12 +11,14 @@ public final class StringSchema extends BaseSchema<String> {
     }
 
     public StringSchema minLength(int n) {
-        addCheck("min_len_" + n, v -> v == null || v.length() >= n);
+        // If value is null or empty AND not required, it's considered valid
+        addCheck("min_len_" + n, v -> v == null || v.isEmpty() || v.length() >= n);
         return this;
     }
 
     public StringSchema contains(String part) {
-        addCheck("contains_" + part, v -> v == null || v.contains(part));
+        // If value is null or empty AND not required, it's considered valid
+        addCheck("contains_" + part, v -> v == null || v.isEmpty() || v.contains(part));
         return this;
     }
 }
